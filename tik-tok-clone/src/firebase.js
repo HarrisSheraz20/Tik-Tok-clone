536c7708a46a600660e68394d7d7488c1e9d1c15
Harris Sheraz <58743620+HarrisSheraz20@users.noreply.github.com>
2021-04-27 02:24:47 +0100

    added firebase and react app.

---
+++ tik-tok-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCHG40bWy6QeWO9EczvFxZe6QDA51Ugs1U",
+  authDomain: "tik-tok-clone-ba5d1.firebaseapp.com",
+  projectId: "tik-tok-clone-ba5d1",
+  storageBucket: "tik-tok-clone-ba5d1.appspot.com",
+  messagingSenderId: "293372660790",
+  appId: "1:293372660790:web:26d4e0602c1489f4965c97",
+  measurementId: "G-G29VLN8GBK",
+};
